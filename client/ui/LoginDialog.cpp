5da76aa37567d5f956941e6567548be8398b7ec1
Hongjun008 <hongjun-ms@outlook.com>
2025-03-02 22:09:02 +0800

    Complete Login. 优化、完善登录逻辑

--- client/ui/LoginDialog.cpp
+++ client/ui/LoginDialog.cpp
@@ -5,13 +5,13 @@
 #include "Client.h"
 #include "tools/AppLog.h"
 
-LoginDialog::LoginDialog(const Client* client) :
+LoginDialog::LoginDialog(Client* client) :
     Window(nullptr, nullptr, "Whisperm")
 , client(client)
 ,ui(new Ui::LoginDialog) {
     this->setWindowModality(Qt::ApplicationModal);
     ui->setupUi(this->centralWidget);
-    this->setFixedSize(this->centralWidget->size());
+    this->setFixedSize(this->centralWidget->size()+QSize(0, this->titlebarWidget->height()));
     connect(client, &Client::ServerSignal_Connected, this, &LoginDialog::Client_onConnected);
     connect(ui->btn_login, &QPushButton::clicked, this, &LoginDialog::UI_onTryLogin);
 }
@@ -69,16 +69,24 @@ void LoginDialog::closeEvent(QCloseEvent* event)
 void LoginDialog::Client_onConnected()
 {
     this->setWindowTitle("🟩 Whisperm");
+    ui->label_information->setText("");
+    mb_online = true;
 }
 
 void LoginDialog::UI_onTryLogin() const
 {
+    if (!mb_online)
+        return ui->label_information->setText("尚未连接到服务器");
+    if (client->get_State() == Client::State::Authenticating)
+        return ui->label_information->setText("正在登录中...");
     const auto uid = ui->input_uid->text();
     const auto pwd = ui->input_pwd->text();
-    const_cast<Client*>(client)->Client_Login(uid, pwd);
+    client->Client_Login(uid, pwd);
+    ui->label_information->setText("正在登录...");
 }
 
 void LoginDialog::AUTH_FAILED(const QString& response)
 {
+    ui->label_information->setText("登录失败 " + response);
     Log("Login Failed:" + response, AppLog::LogLevel::Warning);
 }
